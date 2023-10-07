78c5918d649e3ada14d183242269cd000e2d154d
Nour kayyali <nourkayyali91@gmail.com>
2023-10-07 17:53:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+//const  customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer=2;
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    //return customerName;
+   // console.log(customerName.toUpperCase())
+}
+//upperCaseCustomerName();
+//setBestCustomer();
+function setBestCustomer(){
+ bestCustomer='not bob';
+return bestCustomer;
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    
+    bestCustomer = 'maybe bob';
+  }
+  
+  function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer=5;
+  throw('error');
+  }
+  
+  
+  console.log(changeLeastFavoriteCustomer());
+  
+  
+  
+  
+
+
